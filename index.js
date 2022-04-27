18ec608ba860fa608cef8bfbf360f09585e4948d
abhi94548 <abhishek.m@accubits.com>
2022-04-27 18:03:24 +0530

    upload image

--- index.js
+++ index.js
@@ -32,7 +32,6 @@ app.use(function(req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
 });
 
 
