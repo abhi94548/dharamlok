60bfac6d1a323dbd2737186025fe372e11629825
abhi94548 <abhishek.m@accubits.com>
2022-04-27 21:05:27 +0530

    port changes

--- index.js
+++ index.js
@@ -34,6 +34,6 @@ const routes = require('./routes/routes');
 app.use('/api', routes);
 
 
-app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
+app.listen(2900, () => {
+    console.log(`Server Started at ${2900}`)
 })
