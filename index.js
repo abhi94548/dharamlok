b9a22ee6dc070cae20df93b4e4ad6c587dc2ecb3
abhi94548 <abhishek.m@accubits.com>
2022-05-05 22:49:45 +0530

    cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+var cors = require('cors')
 const mongoString = process.env.DATABASE_URL;
 
 mongoose.connect(mongoString);
@@ -19,6 +20,8 @@ const app = express();
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 
 app.use(function(req, res, next) {
