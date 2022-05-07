dc9fbe4e13d5b7b006ed851b320214f4cb00364d
abhi94548 <abhishek.m@accubits.com>
2022-05-07 23:08:31 +0530

    cors

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 app.use(cors(corsOptions));
 
 
-app.all('/',function(req, res, next) {
+app.all('/api',function(req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
