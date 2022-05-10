99f1c180398bc62f8b20b26b266ecb7c708d2c46
abhi94548 <abhishek.m@accubits.com>
2022-05-10 16:04:22 +0530

    m

--- routes/routes.js
+++ routes/routes.js
@@ -246,21 +246,19 @@ router.get('/getallpost', (req, res) => {
     try{
     	jwt.verify(req.headers.token, 'bootspider', async function(err, user){
         	if (err) throw err;
-			var isLiked = 0;
 			var result = await postModel.find({}).sort([['createdAt', -1]]);
 			var userLikedPosts = await likeModal.find({userId : user.id});
 			for (var i = 0; i < userLikedPosts.length; i++){
 				for (var j = 0; j < result.length; j++){
 					if(userLikedPosts[i].postId == result[j]._id){
 						result[j].isLiked = true
-						isLiked++;
 					}
 					else{
 						result[j].isLiked = false
 					}
 				}
 			  }
-			res.status(200).json({success : true,message: result + 'isLiked' + isLiked})   
+			res.status(200).json({success : true,message: result})   
 		});
     	}
 	catch (error) {
