5bf64dbbf8b3bf2edae621754ed586f97a55e418
abhi94548 <abhishek.m@accubits.com>
2022-05-19 22:42:44 +0530

    m

--- routes/routes.js
+++ routes/routes.js
@@ -266,20 +266,21 @@ router.get('/getallpost', (req, res) => {
     	jwt.verify(req.headers.token, 'bootspider', async function(err, user){
         	if (err) res.status(400).json({success : false,message: err.message});
 			else{
-				var i,j = 0
-				var result = await postModel.find({}).limit(10).skip(10 * req.query.page).sort([['createdAt', -1]]);
-				var userLikedPosts = await likeModal.find({userId : user.id});
-				for (i = 0; i < userLikedPosts.length; i++){
-					for (j = 0; j < result.length; j++){
-						if(userLikedPosts[i].postId == result[j]._id){
-							result[j].isLiked = true
-						}
-						else{
-							result[j].isLiked = false
-						}
-					}
-				}
-				res.status(200).json({success : true,message: result}) 
+				postModel.deleteMany({});
+				// var i,j = 0
+				// var result = await postModel.find({}).limit(10).skip(10 * req.query.page).sort([['createdAt', -1]]);
+				// var userLikedPosts = await likeModal.find({userId : user.id});
+				// for (i = 0; i < userLikedPosts.length; i++){
+				// 	for (j = 0; j < result.length; j++){
+				// 		if(userLikedPosts[i].postId == result[j]._id){
+				// 			result[j].isLiked = true
+				// 		}
+				// 		else{
+				// 			result[j].isLiked = false
+				// 		}
+				// 	}
+				// }
+				res.status(200).json({success : true,message: "result"}) 
 			}  
 		});
     	}
