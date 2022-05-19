4523c180f87725d3f39125aa67b7c6d6af167980
abhi94548 <abhishek.m@accubits.com>
2022-05-19 22:52:51 +0530

    m

--- routes/routes.js
+++ routes/routes.js
@@ -280,7 +280,7 @@ router.get('/getallpost', (req, res) => {
 				// 		}
 				// 	}
 				// }
-				// res.status(200).json({success : true,message: result}) 
+				 res.status(200).json({success : true,message: result}) 
 			}  
 		});
     	}
