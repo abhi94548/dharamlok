8bfd18c7aa30fb026ad7d441953feeec346fd847
abhi94548 <abhishek.m@accubits.com>
2022-05-22 23:09:09 +0530

    m

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,6 +2,7 @@ const Razorpay = require('razorpay');
 const orderModel = require('../models/orderModel');
 const productModel = require('../models/productModel');
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 
 const razorpayInstance = new Razorpay({
@@ -49,8 +50,10 @@ module.exports = {
                 else{
                     let orderId = req.body.orderId;
                     let paymentId = req.body.paymentId;
-                    let signature = req.headers.signature;
-                    const generated_signature = HMAC-SHA256(orderId + "|" + paymentId, '4XgGfyyMkzYHV8QJdXHcCmBt');
+                    let signature = req.body.signature;
+                    let hmac = crypto.createHmac('sha256', '4XgGfyyMkzYHV8QJdXHcCmBt');
+                    hmac.update(orderId + "|" + paymentId);
+                    const generated_signature = hmac.digest('hex');
                     if (generated_signature === signature) {
                         res.status(200).json({success : true, message: "Payment Successful"})
                     }
