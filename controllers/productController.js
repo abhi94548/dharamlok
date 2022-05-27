b2a290d3281815766b531a1f5a1984472ff83f95
abhi94548 <abhishek.m@accubits.com>
2022-05-27 11:07:33 +0530

    m

--- controllers/productController.js
+++ controllers/productController.js
@@ -103,7 +103,7 @@ module.exports = {
             jwt.verify(req.headers.token, 'bootspider', async function(err, user){
                 if (err) res.status(400).json({success : false,message: err.message});
                 var product = productModel.find({title: { $regex: '.*' + req.body.title + '.*' }});
-                res.status(200).json({success : true, message: product})
+                res.status(200).json({success : true, message: product+"hello"})
             });
             }
         catch (error) {
