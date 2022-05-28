deeb9804951df7a509cd72a8d01ec6610ec988fc
abhi94548 <abhishek.m@accubits.com>
2022-05-28 12:27:29 +0530

    m

--- controllers/postController.js
+++ controllers/postController.js
@@ -51,10 +51,10 @@ module.exports = {
             var result = await postModel.find({}).sort([['_id', 'desc']]).lean();
             var token = req.headers.token;
             if(typeof token != 'undefined'){
-                jwt.verify(req.headers.token, 'bootspider', async function(err, user){
+                jwt.verify(req.headers.token, 'bootspider', async function(err, users){
                     if (err) res.status(400).json({success : false,message: err.message});
-                    var userLikedPosts = await likeModal.find({userId : user.id});
-                    count = user.id
+                    var userLikedPosts = await likeModal.find({userId : users.id});
+                    count = users.id
                     for (var i = 0; i < userLikedPosts.length; i++){
                         for (var j = 0; j < result.length; j++){
                             if(userLikedPosts[i].postId == result[j]._id){
