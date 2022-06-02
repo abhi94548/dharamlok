bc13e980d386740aeb57c6a334475019828ba5c5
abhi94548 <abhishek.m@accubits.com>
2022-06-02 14:03:38 +0530

    m

--- controllers/productController.js
+++ controllers/productController.js
@@ -128,7 +128,7 @@ module.exports = {
         try{
             jwt.verify(req.headers.token, 'bootspider', async function(err, user){
                 if (err) res.status(400).json({success : false,message: err.message});
-                var product = await productModel.find({$and: [{title: { $regex: '.*' + req.body.title + '.*' }},
+                var product = await productModel.find({$or: [{title: { $regex: '.*' + req.body.title + '.*' }},
                             {category: { $regex: '.*' + req.body.category + '.*' }},
                             {type: { $regex: '.*' + req.body.type + '.*' }}]}).sort([['_id', 'desc']]);
                 res.status(200).json({success : true, message: product})
