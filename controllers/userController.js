c49da051df4284e3aa16789fe30c51dfd1da538e
abhi94548 <abhishek.m@accubits.com>
2022-06-06 17:11:55 +0530

    m

--- controllers/userController.js
+++ controllers/userController.js
@@ -241,7 +241,7 @@ module.exports = {
                 if (err) res.status(400).json({success : false,message: err.message});
                 else{
                     const users = await userModel.find({userType : 'user'}).select("name").select("email").select("phone")
-                    .select("profileImageUrl").select("description").select("coverImageUrl").select("category").select("userType").select("typeVendor")
+                    .select("profileImageUrl").select("description").select("coverImageUrl").select("category").select("userType").select("typeVendor").select("active")
                     .sort([['_id', -1]]);
                     res.status(200).json({success : true, message: users})
                 }
