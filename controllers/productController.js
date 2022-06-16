a883b24699d05b0cb74ef49e2f14d05e67523658
abhi94548 <abhishek.m@accubits.com>
2022-06-16 11:53:27 +0530

    removed authentication in get all Products

--- controllers/productController.js
+++ controllers/productController.js
@@ -38,11 +38,8 @@ module.exports = {
     },
     myProduct : function(req, res){
         try{
-            jwt.verify(req.headers.token, 'bootspider', async function(err, user){
-                if (err) res.status(400).json({success : false,message: err.message});
-                var product = await productModel.find({userId : user.id}).sort([['_id', 'desc']]);
-                res.status(200).json({success : true,message: product})
-            });
+            var product = await productModel.find({}).sort([['_id', 'desc']]);
+            res.status(200).json({success : true,message: product})
             }
         catch (error) {
             res.status(400).json({success : false,message: error.message})
